e426edd57888041efe363319f560495a04c91118
Jeff Stern <jeffstern.ny@gmail.com>
2021-01-13 16:24:15 -0600

    new license

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = function removeSpaces(string) {
   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
   return string.replace(/\s/g, "");
+  console.log("function complete")
 };
